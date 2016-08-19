3d8f8213b1496dc84ff7584badd28eaed0ef03e1
jsonlitz <jasondotlee@gmail.com>
2016-08-19 14:28:17 -0700

    add posts join table and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-
+  has_many :posts
+  has_many :cities, through: :posts
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
     @user.try(:authenticate, params[:password])
