332aaebe77c957673dc2011e23a151ec5b7c818d
jsonlitz <jasondotlee@gmail.com>
2016-08-19 13:29:24 -0700

    merge changes

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,28 @@
+class CitiesController < ApplicationController
+  def index
+    @cities = City.all
+    render :index
+  end
+
+  def new
+    @city = City.new
+    render :new
+  end
+
+  def create
+    @city = City.create(city_params)
+    redirect_to @city
+  end
+
+  def show
+    @city = City.find_by_id(params[:id])
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:city, :state, :country, :image)
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
   get "/users/new", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
-  get "/login", to: "sessions#new"
-  get "/logout", to: "sessions#destroy"
+  get "/login", to: "sessions#new", as: "user_login"
+  get "/logout", to: "sessions#destroy", as: "user_logout"
   post "/sessions", to: "sessions#create"
   get "/users/:id/edit", to: "users#edit", as: "edit_user"
   patch "/users/:id", to: "users#update"
+
+  get "/cities", to: "cities#index", as: "cities"
+  get "/cities/new", to: "cities#new", as: "new_city"
+  post "/cities", to: "cities#create"
+  get "/cities/:id", to: "cities#show", as: "city"
 end
