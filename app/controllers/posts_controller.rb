25131bd5f734c537915d34e34736598ba03df223
mehgellan <baumegg@gmail.com>
2016-08-19 15:02:19 -0700

    create methods for new posts through cities

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @city = City.find_by_id(params[:city_id])
+    @city.posts.push(post_params)
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 end
