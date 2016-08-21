657fb487280bb71dea79d3f92ef8e9f7714901b0
jsonlitz <jasondotlee@gmail.com>
2016-08-21 10:40:11 -0700

    worked on routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,18 @@ class PostsController < ApplicationController
   end
 
   def create
-    @city = City.find_by_id(params[:city_id])
-    @city.posts.push(post_params)
-    redirect_to city_path(@city)
+    @city = City.find(params[:city_id])
+    new_post = Post.new(post_params)
+    user_id = current_user[:id]
+    new_post[:user_id] = user_id
+    if new_post.save
+      flash[:success] = "Post successfuly created!"
+      city.posts << new_post
+      redirect_to city_path(city)
+    else
+      flash[:error] = new_post.errors.full_messages.join(", ")
+      redirect_to new_user_post_path(user)
+    end
   end
 
   def show
