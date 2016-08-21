534499e1d2e0ebcad8c26c8b0edfd3a20d5dac5f
SwittS <switt.srikulacheep@gmail.com>
2016-08-21 13:17:41 -0700

    add validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
+
   belongs_to :user
   belongs_to :city
+
+  validates :title, presence: true, length: { minimum: 1,  maximum: 200 }
+
+  validates :text, presence: true, length: { minimum: 1, maximum: 10000 }
+
 end
